d7c27d14a0b7ec7ba6f8f8797715ddaa37acda51
arturveres <artur8118@gmail.com>
2019-02-15 18:51:10 +0200

    Adding mixins for unqiue and unique together validations

---
+++ mixins/unique_field_mixin.py
@@ -0,0 +1,46 @@
+from typing import List
+
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.validators import UniqueValidator
+
+
+class UniqueFieldMixin(serializers.ModelSerializer):
+    _unique_validators: List[str] = []
+
+    def _is_unique_validator(self, validator):
+        return isinstance(validator, UniqueValidator)
+
+    def _has_unique_validator(self, field_serializer):
+        for validator in field_serializer.validators:
+            if self._is_unique_validator(validator):
+                return True
+        return None
+
+    def get_fields(self):
+        fields = super().get_fields()
+        for field_name, field_serializer in fields.items():
+            if self._has_unique_validator(field_serializer):
+                self._unique_validators.append(field_name)
+            field_serializer.validators = [
+                validator for validator in field_serializer.validators
+                if not self._is_unique_validator(validator)
+            ]
+        return fields
+
+    def _validate_unique(self, validated_data):
+        for field in self._unique_validators:
+            unique_together_validator = UniqueValidator(self.Meta.model.objects.all())
+            unique_together_validator.set_context(self.fields[field])
+            try:
+                unique_together_validator(validated_data[field])
+            except ValidationError as exc:
+                raise ValidationError({field: exc.detail})
+
+    def create(self, validated_data):
+        self._validate_unique(validated_data)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        self._validate_unique(validated_data)
+        return super().update(instance,validated_data)
---
+++ mixins/unique_together_mixin.py
@@ -0,0 +1,50 @@
+from typing import List, Tuple
+
+from django.db.models import QuerySet
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import empty
+from rest_framework.validators import UniqueTogetherValidator
+
+
+class UniqueTogetherMixin(serializers.ModelSerializer):
+    _unique_together_validators: List[Tuple[str]] = []
+
+    def __init__(self, instance=None, data=empty, **kwargs):
+        for validator in self.validators:
+            if self._is_unique_together_validator(validator):
+                self._unique_together_validators.append(validator.fields)
+        self.validators = [validator for validator in self.validators
+                           if not self._is_unique_together_validator(validator)]
+        super().__init__(instance, data, **kwargs)
+
+    def _is_unique_together_validator(self, validator):
+        return isinstance(validator, UniqueTogetherValidator)
+
+    def _validate_unique_together_instance(self, validated_data):
+        for fields in self._unique_together_validators:
+            unique_together_validator = UniqueTogetherValidator(self.Meta.model.objects.all(),
+                                                                fields)
+            unique_together_validator.set_context(self)
+            try:
+                unique_together_validator(validated_data)
+            except ValidationError as exc:
+                raise ValidationError({"non_field_errors": exc.detail})
+
+    def _validate_unique_together(self, validated_data):
+        if isinstance(self.instance, QuerySet):
+            queryset = self.instance
+            for item in self.instance.all():
+                self.instance = item
+                self._validate_unique_together_instance(validated_data)
+            self.instance = queryset
+        else:
+            self._validate_unique_together_instance(validated_data)
+
+    def create(self, validated_data):
+        self._validate_unique_together(validated_data)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        self._validate_unique_together(validated_data)
+        return super().update(instance, validated_data)
