48d58ea488e0722cc0b35fbbe93d1f8f034f0217
arturveres <artur8118@gmail.com>
2019-02-26 16:13:50 +0200

    Changin version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="drf_nested",
-    version="0.1.2",
+    version="0.1.3",
     author="Andréas Kühne, Artur Veres",
     author_email="andreas.kuhne@promoteint.com, artur8118@gmail.com",
     maintainer="Artur Veres",
