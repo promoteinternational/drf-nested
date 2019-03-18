01b947e77be0893aa042286bb44ffd11e98faa41
arturveres <artur8118@gmail.com>
2019-03-18 12:25:15 +0200

    Fixing examples

--- examples/nested_example/nested_example/settings.py
+++ examples/nested_example/nested_example/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.append(os.path.dirname(os.path.dirname(BASE_DIR)))
 
 
 # Quick-start development settings - unsuitable for production
