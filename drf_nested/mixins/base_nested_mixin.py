5eb83befb0bf617df2f03eb30c111d531753e2b4
Artur Veres <artur8118@gmail.com>
2019-12-12 13:07:19 +0200

    Reworking init

--- drf_nested/mixins/base_nested_mixin.py
+++ drf_nested/mixins/base_nested_mixin.py
@@ -19,41 +19,33 @@ class BaseNestedMixin(serializers.ModelSerializer):
     Provides all the needed methods and properties for manipulating nested data.
     """
 
-    # def __init__(self, instance=None, data: Union[empty, dict, list] = empty, **kwargs):
-    #     super().__init__(instance, data, **kwargs)
-    #
-    #     if data is not empty and not isinstance(data, list) and data is not None:
-    #         data, nested_fields_data = self._get_nested_fields(data)
-    #
-    #         # For all nested fields, initialize serializer with model instances
-    #         # for further unique/unique_together checks
-    #         for field_name, nested_data in nested_fields_data.items():
-    #             serializer = self.fields.get(field_name)
-    #             if serializer:
-    #                 serializer_kwargs = (serializer.child._kwargs if "child" in serializer._kwargs
-    #                                      else serializer._kwargs)
-    #                 serializer_kwargs.update(context=self.context,
-    #                                          data=nested_data)
-    #
-    #                 # Finding either single pk value or list of them,
-    #                 # in case the nested serializer is a ListSerializer instance
-    #                 pk = self._get_field_pk_value(field_name, nested_data)
-    #
-    #                 # Initializing the new instances nested serializers with model instances
-    #                 if issubclass(serializer.__class__, ListSerializer):
-    #                     serializer = serializer.child.__class__
-    #                     model = serializer.Meta.model
-    #                     serializer_kwargs.update({"many": True})
-    #                     model_instance = model.objects.filter(**{"pk__in": pk or []})
-    #                 else:
-    #                     serializer = serializer.__class__
-    #                     model = serializer.Meta.model
-    #                     model_instance = model.objects.filter(**{"pk": pk}).first()
-    #
-    #                 # Replacing old serializer with new one, that is populated with model instances
-    #                 del self.fields[field_name]
-    #                 new_serializer = serializer(model_instance, **serializer_kwargs)
-    #                 self.fields[field_name] = new_serializer
+    def __init__(self, instance=None, data: Union[empty, dict, list] = empty, **kwargs):
+        super().__init__(instance, data, **kwargs)
+
+        if data is not empty and not isinstance(data, list) and data is not None:
+            data, nested_fields_data = self._get_nested_fields(data)
+
+            # For all nested fields, initialize serializer with model instances
+            # for further unique/unique_together checks
+            for field_name, nested_data in nested_fields_data.items():
+                serializer = self.fields.get(field_name)
+                if serializer:
+                    serializer_kwargs = (serializer.child._kwargs if "child" in serializer._kwargs
+                                         else serializer._kwargs)
+                    serializer_kwargs.update(context=self.context,
+                                             data=nested_data)
+
+                    # Initializing the new instances nested serializers with model instances
+                    if issubclass(serializer.__class__, ListSerializer):
+                        serializer = serializer.child.__class__
+                        serializer_kwargs.update({"many": True})
+                    else:
+                        serializer = serializer.__class__
+
+                    # Replacing old serializer with new one, that is populated with data
+                    del self.fields[field_name]
+                    new_serializer = serializer(**serializer_kwargs)
+                    self.fields[field_name] = new_serializer
 
     def _get_field_pk_value(self, field_name: str, nested_data):
         """
