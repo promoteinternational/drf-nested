bcf5dbe1700565895f1fd7640d53a002cd4b8177
Artur Veres <artur8118@gmail.com>
2019-12-18 15:56:03 +0200

    Fixing nested validation for serializers without attached instances

--- drf_nested/utils/queryset_to_instance.py
+++ drf_nested/utils/queryset_to_instance.py
@@ -35,6 +35,9 @@ class QuerySetInstanceManager:
         if self.serializer_instance.instance is not None and isinstance(self.serializer_instance.instance, QuerySet):
             self.serializer_instance._set_instance(self.validated_data,
                                                    self.original_instance)
+        else:
+            self.serializer_instance._set_instance(self.validated_data,
+                                                   self.serializer_instance.Meta.model.objects.all())
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.serializer_instance.instance = self.original_instance
